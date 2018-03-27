56bd1e659f56c5d9fce66f407ec6241cb76ceb45
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2018-03-28 01:26:05 +0530

    Initial commit

---
+++ app/src/main/gen/demo/example/com/customarrayadapter/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package demo.example.com.customarrayadapter;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
